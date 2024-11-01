975fb97c48b5e34a75c04c77780a39857bffe2bf
SantiRDL <santiago.rodriguezdl@ucu.edu.uy>
2024-11-01 08:55:10 -0300

    a

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,15 @@
+// Code for the card component
+import './App.css';
+
+function Card() {
+    return (
+    <div className="Card">
+        <header className="Card-header">
+            <button className="Card-button">Button 1</button>
+            <button className="Card-button">Button 2</button>
+        </header>
+    </div>
+    );
+}
+
+export default Card;
